27b041ded9039329f1568506c9330968d6f22bdb
Zng Tfy <nvt87x@gmail.com>
2018-06-12 10:58:29 +0700

    First commit

---
+++ src/main/java/com/tva/fe/FootballEventApplication.java
@@ -0,0 +1,12 @@
+package com.tva.fe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballEventApplication.class, args);
+	}
+}
